6f1275c77b4e4b7eff1af0f53627e059f7e4b9ac
Fouche du Preez <fouche@limehouse.co.za>
2025-03-01 21:22:41 +0200

    added jQuery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document).on("turbo:load", function (e) {
+    hideShowAddress();
+});
+
+function hideShowAddress() {
+    $("#address-toggle").on("change", function (e) {
+        $("#address-details").toggleClass("d-none");
+
+        if ($("#lblShowHide").text() === "Hide Address") {
+            $("#lblShowHide").text("Show Address");
+        } else {
+            $("#lblShowHide").text("Hide Address");
+        }
+    })
+
+}
